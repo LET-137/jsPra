dec7213fccb9122d1cf9142db7bdf1d420ca968d
LET <bjs.6300.137@gmail.com>
2024-06-25 21:52:14 +0900

    遷移完了までクリックを無効

--- dist/portfolio/js/script.js
+++ dist/portfolio/js/script.js
@@ -1,6 +1,7 @@
 const text = document.querySelector('.title');
 const menus = document.querySelectorAll('.menu');
 const contentContainer = document.querySelectorAll('.contents-container');
+const topPage = document.querySelector('#top');
 const titleText = 'Takuya Tsumoto PorTfolio'
 
 // 文字を受け取り、<span>要素を作成する関数
@@ -60,67 +61,74 @@ const secondMoveTitle = () => {
   });
   // メニューを表示
   for (let i = 0; i < menus.length; i++) {
-    console.log(i);
     menus[i].animate(keyframes, options);
+    console.log('menu')
   };
+  topPage.animate(keyframes, options);
 };
 
 // タイトルとメニューを表示
 const moveTitleFunctions = [
-  firstMoveTitle,
-  secondMoveTitle
+  { func: firstMoveTitle },
+  { func: secondMoveTitle },
 ];
 
+// 非同期で指定した関数を実行
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-const executeFunctions = async (func, time) => {
-  for (let i = 0; i < moveTitleFunctions.length; i++) {
-    func[i]();
+const executeFunctions = async (funcs, time) => {
+  for (let i = 0; i < funcs.length; i++) {
+    const { func, args = [] } = funcs[i];
+    func(...args);
     await delay(time);
   }
 };
-
 executeFunctions(moveTitleFunctions, 3000);
 
-const moveCerterColumn = (startOpa, endOpa,startDir,endDir, element) => {
+// 遷移時のアニメーション
+const moveCerterColumn = (startOpa, endOpa,startDir,endDir, element, duration) => {
   const keyframes = {
     opacity: [startOpa, endOpa],
     translate: [`${startDir}`, `${endDir}`],
   }
   const options = {
-    duration: 1000,
+    duration: duration,
     easing: 'ease',
     fill: 'forwards'
   } 
   element.animate(keyframes, options);
 }
 
-// ページ遷移
-function showPage(pageID) {
-  const closePage = () => {
-    contentContainer.forEach((content) => {
-      moveCerterColumn(1,0,'0','5%',content);
-    });
-  };
-  const openPage = () => {
-    contentContainer.forEach((content) => {
-      moveCerterColumn(0,1,'5%','0',content);
-    });
-  };
-
-  const funcPages = [
-    closePage,
-    openPage
-  ];
-  executeFunctions(funcPages, 1000);
-
-  setTimeout(() => {
-    let pages = document.querySelectorAll('.page');
+// 遷移時にactiveクラスを操作
+function pageTransition(pageID) {
+  let pages = document.querySelectorAll('.page');
   pages.forEach(function(page) {
     page.classList.remove('active');
   });
   let activePage = document.getElementById(pageID);
   activePage.classList.add('active');
-  } ,1000);
   
-  
+};
+
+// ページ遷移
+function showPage(pageID) {
+  const activePage = document.getElementById(`navi-${pageID}`);
+  activePage.style.pointerEvents = 'none';
+  const closePage = () => {
+    contentContainer.forEach((content) => {
+      moveCerterColumn(1,0,'0','5%',content, 700);
+    });
+  };
+  const openPage = () => {
+    contentContainer.forEach((content) => {
+      moveCerterColumn(0,1,'5%','0',content, 700);
+    });
+    activePage.style.pointerEvents = 'auto';
+  };
+
+  const funcPages = [
+    { func: closePage },
+    { func: pageTransition, args: [pageID] },
+    { func: openPage },
+  ];
+  executeFunctions(funcPages, 800,pageTransition, pageID);
 }
